f82bc8ee34dccbc5be2fee7f23e36c6f3bc84a27
javbanda <jav.banda@duocuc.cl>
2026-09-11 23:56:59 -0300

    Se actualiza el app routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
 import { Pedidos } from './pedidos/pedidos';
 
 export const routes: Routes = [
-    {path:'', redirectTo: 'pedidos', pathMatch: 'full'},
-    {path: 'pedidos', component: Pedidos},
-    {path: '**', redirectTo:'pedidos'}
+  { path: '', component: Home },
+  { path: 'pedidos', component: Pedidos },
+  { path: '**', redirectTo: '' },
 ];
