6927a2f1ac7e38bf06bca65275486eaad8f42c71
javbanda <jav.banda@duocuc.cl>
2026-09-15 11:01:46 -0300

    Se crea el environment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Pedidos } from './pedidos/pedidos';
+import { Login } from './login/login';
 
 export const routes: Routes = [
   { path: '', component: Home },
+  { path: 'login', component: Login },
   { path: 'pedidos', component: Pedidos },
   { path: '**', redirectTo: '' },
 ];
