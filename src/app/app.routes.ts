e174fd6247dadf0778c0abb6fd1eba6aa6cc1350
javbanda <jav.banda@duocuc.cl>
2026-09-23 17:59:21 -0300

    Se agrega el import y el path de reportes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Pedidos } from './pedidos/pedidos';
 import { Login } from './login/login';
+import { Reportes } from './reportes/reportes';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'login', component: Login },
   { path: 'pedidos', component: Pedidos },
+  { path: 'reportes', component: Reportes },
   { path: '**', redirectTo: '' },
 ];
