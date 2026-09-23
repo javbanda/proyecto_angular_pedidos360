f866f682dd95edae88498e2d44feeb4c713ebcc3
javbanda <jav.banda@duocuc.cl>
2026-09-23 16:43:45 -0300

    Se actualiza el reportes.ts

--- src/app/reportes/reportes.ts
+++ src/app/reportes/reportes.ts
@@ -1,9 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ReporteService } from './reporteservice';
 
 @Component({
   selector: 'app-reportes',
+  standalone: true,
   imports: [],
   templateUrl: './reportes.html',
-  styleUrl: './reportes.css',
+  styleUrl: './reportes.css'
 })
-export class Reportes {}
+export class Reportes implements OnInit {
+
+  private reportesService = inject(ReporteService);
+
+  kpis = signal('');
+  estadosActivos = signal<any[]>([]);
+  leadTime = signal<any>(null);
+  ventasPorHora = signal<any[]>([]);
+
+  ngOnInit(): void {
+
+    this.reportesService.obtenerKpis().subscribe({
+      next: (datos) => {
+        console.log('KPIs:', datos);
+        this.kpis.set(datos);
+      },
+      error: (error) => {
+        console.error('Error KPIs:', error);
+      }
+    });
+
+    this.reportesService.obtenerEstadosActivos().subscribe({
+      next: (datos) => {
+        console.log('Estados activos:', datos);
+        this.estadosActivos.set(datos);
+      },
+      error: (error) => {
+        console.error('Error estados:', error);
+      }
+    });
+
+    this.reportesService.obtenerLeadTime().subscribe({
+      next: (datos) => {
+        console.log('Lead time:', datos);
+        this.leadTime.set(datos);
+      },
+      error: (error) => {
+        console.error('Error lead time:', error);
+      }
+    });
+
+    this.reportesService.obtenerVentasPorHora().subscribe({
+      next: (datos) => {
+        console.log('Ventas por hora:', datos);
+        this.ventasPorHora.set(datos);
+      },
+      error: (error) => {
+        console.error('Error ventas por hora:', error);
+      }
+    });
+  }
+}
