51b4adc406ce395e21cd292e9defbe54c611bab9
javbanda <jav.banda@duocuc.cl>
2026-09-23 16:27:50 -0300

    Se crea el componente reportes

--- src/app/pedidos/pedidoservice.ts
+++ src/app/pedidos/pedidoservice.ts
@@ -7,10 +7,11 @@ import { Pedido } from '../interfaces/pedido.model';
   providedIn: 'root'
 })
 export class PedidoService {
+
   private http = inject(HttpClient);
-  // private apiUrl = 'http://localhost:8080/api/pedidos';
-  //private apiUrl = 'http://pedidos-api-env.eba-a97dmzri.us-east-1.elasticbeanstalk.com/api/pedidos'
-  private apiUrl = 'https://8s8pz6vmm3.execute-api.us-east-1.amazonaws.com/prod/api/pedidos'
+
+  private apiUrl = 'http://localhost:8080/api/pedidos';
+
   obtenerPedidos(): Observable<Pedido[]> {
     return this.http.get<Pedido[]>(this.apiUrl);
   }
