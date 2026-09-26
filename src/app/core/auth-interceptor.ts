58c0c553df86b337a6f4654f87087da9cb157eac
javbanda <jav.banda@duocuc.cl>
2026-09-25 22:43:30 -0300

    Se agrega el interceptor de autenticacion

--- src/app/core/auth-interceptor.ts
+++ src/app/core/auth-interceptor.ts
@@ -1,5 +1,18 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+
+  const accessToken = localStorage.getItem('accessToken');
+
+  if (accessToken) {
+    const reqConToken = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    });
+
+    return next(reqConToken);
+  }
+
   return next(req);
 };
